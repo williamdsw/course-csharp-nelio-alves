f9df95914998e1abf43d560ca5df9648cbd52174
williamdsw <williamdsw@outlook.com>
2020-07-22 19:18:32 -0300

    Data Input #2

--- CourseNelioAlves/Program.cs
+++ CourseNelioAlves/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Dynamic;
+using System.Globalization;
 
 namespace CourseNelioAlves
 {
@@ -7,22 +7,24 @@ namespace CourseNelioAlves
     {
         static void Main(string[] args)
         {
-            string phrase = Console.ReadLine();
-            string x = Console.ReadLine();
-            string y = Console.ReadLine();
-            string z = Console.ReadLine();
-            string[] words = Console.ReadLine().Split(' ');
+            int number = int.Parse(Console.ReadLine());
+            char ch = char.Parse(Console.ReadLine());
+            double other = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            string[] values = Console.ReadLine().Split(' ');
+            string name = values[0];
+            char genre = char.Parse(values[1]);
+            int age = int.Parse(values[2]);
+            double height = double.Parse(values[3], CultureInfo.InvariantCulture);
 
-            Console.WriteLine("\nInput: ");
-            Console.WriteLine(phrase);
-            Console.WriteLine(x);
-            Console.WriteLine(y);
-            Console.WriteLine(z);
-            
-            foreach (string word in words)
-            {
-                Console.WriteLine($"word: {word}");
-            }
+            Console.WriteLine("\nInput:");
+            Console.WriteLine(number);
+            Console.WriteLine(ch);
+            Console.WriteLine(other);
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine(name);
+            Console.WriteLine(genre);
+            Console.WriteLine(age);
+            Console.WriteLine(height);
         }
     }
 }
