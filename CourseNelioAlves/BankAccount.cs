85eb9704fbd7aefcc9f2c72350578fc4257bf590
williamdsw <williamdsw@outlook.com>
2020-07-22 18:09:48 -0300

    Naming convention

---
+++ CourseNelioAlves/BankAccount.cs
@@ -0,0 +1,19 @@
+﻿
+namespace CourseNelioAlves
+{
+    class BankAccount
+    {
+        private double _balance;
+        public string Owner { get; set; }
+
+        public void Deposit (double quantity)
+        {
+            _balance += quantity;
+        }
+
+        public double GetBalance ()
+        {
+            return this._balance;
+        }
+    }
+}
