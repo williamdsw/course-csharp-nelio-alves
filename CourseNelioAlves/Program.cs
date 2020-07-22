b9cca25ac693e734edcc519705f49caa5e6849c3
williamdsw <williamdsw@outlook.com>
2020-07-22 17:50:25 -0300

    Basic types in C# pt-1

---
+++ CourseNelioAlves/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CourseNelioAlves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte mySbyte = 100;
+            SByte myOtherSByte = 100;
+            byte myByte = 126;
+            int myInteger = 1000;
+            long myLong = 27654762543L;
+
+            Console.WriteLine($"sbyte: {mySbyte}");
+            Console.WriteLine($"SByte: {myOtherSByte}");
+            Console.WriteLine($"byte: {myByte}");
+            Console.WriteLine($"int: {myInteger}");
+            Console.WriteLine($"long: {myLong}");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# course-csharp-nelio-alves
