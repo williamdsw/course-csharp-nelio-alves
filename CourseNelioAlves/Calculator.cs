ed936e158aede10b107d8942a5bb6e93cf19ee66
williamdsw <williamdsw@outlook.com>
2020-07-23 16:48:22 -0300

    Static members #2

---
+++ CourseNelioAlves/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourseNelioAlves
+{
+    class Calculator
+    {
+        public static double Circumference(double radius)
+        {
+            return 2.0 * Math.PI * radius;
+        }
+
+        public static double Volume(double radius)
+        {
+            return 4.0 / 3.0 * Math.PI * Math.Pow(radius, 3);
+        }
+    }
+}
--- CourseNelioAlves/Program.cs
+++ CourseNelioAlves/Program.cs
@@ -6,28 +6,18 @@ namespace CourseNelioAlves
 {
     class Program
     {
-        static double PI = 3.14;
-
         static void Main(string[] args)
         {
             Console.Write("Input radius value: ");
             double radius = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            double circumference = Circumference(radius);
-            double volume = Volume(radius);
+            double circumference = Calculator.Circumference(radius);
+            double volume = Calculator.Volume(radius);
 
             Console.WriteLine("Circumference: {0}", circumference.ToString ("F2", CultureInfo.InvariantCulture));
             Console.WriteLine("Volume: {0}", volume.ToString ("F2", CultureInfo.InvariantCulture));
-            Console.WriteLine("PI: {0}", PI.ToString ("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PI: {0}", Math.PI.ToString ("F2", CultureInfo.InvariantCulture));
         }
 
-        private static double Circumference (double radius)
-        {
-            return 2.0 * PI * radius;
-        }
-
-        private static double Volume (double radius)
-        {
-            return 4.0 / 3.0 * PI * Math.Pow(radius, 3);
-        }
+        
     }
 }
