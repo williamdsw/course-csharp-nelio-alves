d9d1a114297c0a0c10ddc411e43cf1f6a8bab711
williamdsw <williamdsw@outlook.com>
2020-07-23 15:30:29 -0300

    Creating a class to represent triangule

--- CourseNelioAlves/Program.cs
+++ CourseNelioAlves/Program.cs
@@ -7,38 +7,37 @@ namespace CourseNelioAlves
     {
         static void Main(string[] args)
         {
-            double xA, xB, xC, yA, yB, yC;
+            Console.Write("Input the number of triangule that you want: ");
+            int number = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Input the values of triangule X:");
-            xA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            xB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            xC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            Console.WriteLine("Input the values of triangule Y:");
-            yA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            yB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            yC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            double p = (xA + xB + xC) / 2;
-            double areaX = Math.Sqrt(p * (p - xA) * (p - xB) * (p - xC));
-
-            p = (yA + yB + yC) / 2;
-            double areaY = Math.Sqrt(p * (p - yA) * (p - yB) * (p - yC));
-
-            Console.WriteLine("X triangule area: {0}", areaX.ToString ("F4", CultureInfo.InvariantCulture));
-            Console.WriteLine("Y triangule area: {0}", areaY.ToString ("F4", CultureInfo.InvariantCulture));
-
-            if (areaX > areaY)
+            if (number >= 1)
             {
-                Console.WriteLine("X triangule has greater area");
-            }
-            else if (areaX < areaY)
-            {
-                Console.WriteLine("Y triangule has greater area");
-            }
-            else
-            {
-                Console.WriteLine("Both triangules have same area");
+                int greatestTriangule = 0;
+                double greatestArea = 0;
+
+                for (int i = 1; i <= number; i++)
+                {
+                    Console.WriteLine($"Input values of {i}# triangule:");
+                    double a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    double b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    double c = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    Triangle triangle = new Triangle(a, b, c);
+                    if (greatestTriangule == 0)
+                    {
+                        greatestTriangule++;
+                        greatestArea = triangle.GetArea();
+                    }
+                    else if (triangle.GetArea() > greatestArea)
+                    {
+                        greatestArea = triangle.GetArea();
+                        greatestTriangule = i;
+                    }
+
+                    Console.WriteLine($"Area: {triangle.GetArea ().ToString ("F4", CultureInfo.InvariantCulture)}");
+                }
+
+                Console.WriteLine($"{greatestTriangule}º triangule is the greatest with area of {greatestArea.ToString ("F4", CultureInfo.InvariantCulture)}");
             }
         }
     }
---
+++ CourseNelioAlves/Triangle.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourseNelioAlves
+{
+    class Triangle
+    {
+        private double a;
+        private double b;
+        private double c;
+
+        public Triangle() { }
+        public Triangle(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public double getA()
+        {
+            return this.a;
+        }
+
+        public void setA(double a)
+        {
+            this.a = a;
+        }
+        public double getB()
+        {
+            return this.b;
+        }
+
+        public void setB(double b)
+        {
+            this.b = b;
+        }
+        public double getC()
+        {
+            return this.c;
+        }
+
+        public void setC(double c)
+        {
+            this.c = c;
+        }
+
+        public double GetArea ()
+        {
+            double p = (a + b + c) / 2;
+            double area = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            return area;
+        }
+    }
+}
