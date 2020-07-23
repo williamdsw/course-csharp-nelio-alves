bee6ae6d724eb90fa8a68cd521b44e4ceb11f36d
williamdsw <williamdsw@outlook.com>
2020-07-23 11:29:32 -0300

    Comparatives operators

--- CourseNelioAlves/Program.cs
+++ CourseNelioAlves/Program.cs
@@ -7,24 +7,25 @@ namespace CourseNelioAlves
     {
         static void Main(string[] args)
         {
-            int number = int.Parse(Console.ReadLine());
-            char ch = char.Parse(Console.ReadLine());
-            double other = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            string[] values = Console.ReadLine().Split(' ');
-            string name = values[0];
-            char genre = char.Parse(values[1]);
-            int age = int.Parse(values[2]);
-            double height = double.Parse(values[3], CultureInfo.InvariantCulture);
+            int a = 10;
+            bool isLessThanTen = a < 10;
+            bool isLessThanTwenty = a < 20;
+            bool isGreaterThanTen = a > 10;
+            bool isGreaterThanFive = a > 5;
+            bool isLessOrEqualToTen = a <= 10;
+            bool isGreaterOrEqualToTen = a >= 10;
+            bool isEqualsToTen = a == 10;
+            bool isDifferentOfTen = a != 10;
 
-            Console.WriteLine("\nInput:");
-            Console.WriteLine(number);
-            Console.WriteLine(ch);
-            Console.WriteLine(other);
-            Console.WriteLine("------------------------------------");
-            Console.WriteLine(name);
-            Console.WriteLine(genre);
-            Console.WriteLine(age);
-            Console.WriteLine(height);
+            Console.WriteLine($"a = {a}");
+            Console.WriteLine($"a < 10 ? {isLessThanTen}");
+            Console.WriteLine($"a < 20 ? {isLessThanTwenty}");
+            Console.WriteLine($"a > 10 ? {isGreaterThanTen}");
+            Console.WriteLine($"a > 5 ? {isGreaterThanFive}");
+            Console.WriteLine($"a <= 10 ? {isLessOrEqualToTen}");
+            Console.WriteLine($"a >= 10 ? {isGreaterOrEqualToTen}");
+            Console.WriteLine($"a == 10 ? {isEqualsToTen}");
+            Console.WriteLine($"a != 10 ? {isDifferentOfTen}");
         }
     }
 }
