343cb999247f0af1170089bca314d412f4d26c7b
williamdsw <williamdsw@outlook.com>
2020-07-24 16:30:32 -0300

    List #2

--- CourseNelioAlves/Program.cs
+++ CourseNelioAlves/Program.cs
@@ -7,8 +7,64 @@ namespace CourseNelioAlves
     {
         static void Main(string[] args)
         {
-            List<string> firstList = new List<string>();
-            List<string> secondList = new List<string>() { "Maria", "Alex" };
+            List<string> teams = new List<string>();
+
+            teams.Add("Real Madrid");
+            teams.AddRange(new string[] { "Barcelona", "Manchester City", "Atalanta", "Manchester United" });
+            teams.Insert(1, "Chelsea");
+
+            foreach (string team in teams)
+            {
+                Console.WriteLine(team);
+            }
+
+            Console.WriteLine("---------------------------");
+            Console.WriteLine($"List count: {teams.Count}");
+
+            string firstWithM = teams.Find(team => team.StartsWith("M"));
+            int firstIndex = teams.FindIndex(team => team.StartsWith("M"));
+            Console.WriteLine($"Find first with M: {firstWithM}");
+            Console.WriteLine($"Find index first with M: {firstIndex}");
+
+            string lastWithM = teams.FindLast(team => team.StartsWith("M"));
+            int lastIndex = teams.FindLastIndex(team => team.StartsWith("M"));
+            Console.WriteLine($"Find last with M: {lastWithM}");
+            Console.WriteLine($"Find last index first with M: {lastIndex}");
+
+            Console.WriteLine("---------------------------");
+            List<string> endsWithAList = teams.FindAll(team => team.EndsWith("a"));
+            foreach (var team in endsWithAList)
+            {
+                Console.WriteLine(team);
+            }
+
+            Console.WriteLine("---------------------------");
+            endsWithAList.Remove("Chelsea");
+            foreach (var team in endsWithAList)
+            {
+                Console.WriteLine(team);
+            }
+
+            Console.WriteLine("---------------------------");
+            teams.RemoveAll(team => team.Contains(" "));
+            foreach (string team in teams)
+            {
+                Console.WriteLine(team);
+            }
+
+            Console.WriteLine("---------------------------");
+            endsWithAList.RemoveAt(0);
+            foreach (string team in endsWithAList)
+            {
+                Console.WriteLine(team);
+            }
+
+            Console.WriteLine("---------------------------");
+            teams.RemoveRange(1, 2);
+            foreach (string team in teams)
+            {
+                Console.WriteLine(team);
+            }
         }
     }
 }
