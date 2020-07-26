55139755e9d7a4657b95d90facd39493742f25dd
williamdsw <williamdsw@outlook.com>
2020-07-26 19:18:13 -0300

    String functions

--- CourseNelioAlves/Program.cs
+++ CourseNelioAlves/Program.cs
@@ -6,10 +6,37 @@ namespace CourseNelioAlves
     {
         static void Main(string[] args)
         {
-            Console.Write("Input your age: ");
-            int age = int.Parse(Console.ReadLine());
-            bool canDrive = (age >= 18);
-            Console.WriteLine($"Can you get a license driver? {canDrive}");
+            string phrase = "   I see the bad moon on rising. I see trouble on the way.   ";
+            string upper = phrase.ToUpper();
+            string lower = phrase.ToLower();
+            int length = phrase.Length;
+            string trim = phrase.Trim();
+            int firstIndexOf = phrase.IndexOf("see");
+            int lastIndexOf = phrase.LastIndexOf("see");
+            string substring = phrase.Substring(10);
+            string otherSubstring = phrase.Substring(3, 10);
+            string replace = phrase.Replace("see", "saw");
+            bool nullOrEmpty = string.IsNullOrEmpty(phrase);
+            bool nullOrWhiteSpace = string.IsNullOrWhiteSpace(phrase);
+
+            Console.WriteLine(phrase);
+            Console.WriteLine(upper);
+            Console.WriteLine(lower);
+            Console.WriteLine($"Length: {length}");
+            Console.WriteLine(trim);
+            Console.WriteLine($"IndexOf(see): {firstIndexOf}");
+            Console.WriteLine($"LastIndexOf(see): {lastIndexOf}");
+            Console.WriteLine($"Substring(3): {substring}");
+            Console.WriteLine($"Substring(3, 10): {otherSubstring}");
+            Console.WriteLine(replace);
+            Console.WriteLine($"IsNullOrEmpty: {nullOrEmpty}");
+            Console.WriteLine($"IsNullOrWhitespace: {nullOrWhiteSpace}");
+            Console.WriteLine("------------------------------------------");
+
+            foreach (char c in phrase.Trim(' '))
+            {
+                Console.Write($" {c} ");
+            }
         }
     }
 }
