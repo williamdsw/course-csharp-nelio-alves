764dff24634b70b21288aa47606920f85d563bbb
williamdsw <williamdsw@outlook.com>
2020-07-26 19:06:57 -0300

    Ternary condition expression

--- CourseNelioAlves/Program.cs
+++ CourseNelioAlves/Program.cs
@@ -6,23 +6,10 @@ namespace CourseNelioAlves
     {
         static void Main(string[] args)
         {
-            Console.Write("Input an integer: ");
-            int number = int.Parse(Console.ReadLine());
-            string day;
-
-            switch (number)
-            {
-                case 1: { day = "Sunday"; break; }
-                case 2: { day = "Monday"; break; }
-                case 3: { day = "Tuesday"; break; }
-                case 4: { day = "Wednesday"; break; }
-                case 5: { day = "Thursday"; break; }
-                case 6: { day = "Friday"; break; }
-                case 7: { day = "Saturday"; break; }
-                default: { day = "Invalid Number"; break; }
-            }
-
-            Console.WriteLine($"day: {day}");
+            Console.Write("Input your age: ");
+            int age = int.Parse(Console.ReadLine());
+            bool canDrive = (age >= 18);
+            Console.WriteLine($"Can you get a license driver? {canDrive}");
         }
     }
 }
