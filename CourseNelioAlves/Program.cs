03f25152e32a6b9a937ec9fcd08aaef585462a96
williamdsw <williamdsw@outlook.com>
2020-07-26 18:58:51 -0300

    Type inference: keyword 'var'

--- CourseNelioAlves/Program.cs
+++ CourseNelioAlves/Program.cs
@@ -6,55 +6,13 @@ namespace CourseNelioAlves
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Multiplication Table");
-            Console.Write("Input quantity of numbers: ");
-            int quantity = int.Parse(Console.ReadLine()); 
-            Console.Write("Input the number of ocorrences: ");
-            int ocorrences = int.Parse(Console.ReadLine());
+            var x = 10;
+            var y = 20.0;
+            var z = "Maria";
 
-            int[,] values = new int[quantity, ocorrences];
-            for (int row = 0; row < quantity; row++)
-            {
-                for (int col = 0; col < ocorrences; col++)
-                {
-                    values[row, col] = row * col;
-                }
-            }
-
-            // Header
-            Console.Write("\t");
-            for (int i = 0; i < ocorrences; i++)
-            {
-                Console.Write($" { i } ");
-            }
-
-            Console.WriteLine('\n');
-            for (int row = 0; row < values.GetLength(0); row++)
-            {
-                // Column count
-                Console.Write($" {row}: \t");
-
-                // Data
-                for (int col = 0; col < values.GetLength(1); col++)
-                {
-                    Console.Write($" {values[row, col]} ");
-                }
-
-                Console.WriteLine();
-            }
-
-            Console.WriteLine($"\nRank: {values.Rank}");
-            Console.WriteLine($"Length: {values.Length}");
-            Console.WriteLine($"Length of 0: {values.GetLength(0)}");
-            Console.WriteLine($"Length of 1: {values.GetLength(1)}");
-            Console.WriteLine("---------------------------------------------");
-
-            Console.WriteLine("Main diagonal: ");
-            for (int i = 0; i < quantity; i++)
-            {
-                Console.Write($" {values[i, i]} ");
-            }
-
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
         }
     }
 }
