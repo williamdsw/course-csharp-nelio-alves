ffdf96edb36f64f567e7ce3990ebfd618bc8884a
williamdsw <williamdsw@outlook.com>
2020-07-26 19:03:07 -0300

    switch-case

--- CourseNelioAlves/Program.cs
+++ CourseNelioAlves/Program.cs
@@ -6,13 +6,23 @@ namespace CourseNelioAlves
     {
         static void Main(string[] args)
         {
-            var x = 10;
-            var y = 20.0;
-            var z = "Maria";
+            Console.Write("Input an integer: ");
+            int number = int.Parse(Console.ReadLine());
+            string day;
 
-            Console.WriteLine(x);
-            Console.WriteLine(y);
-            Console.WriteLine(z);
+            switch (number)
+            {
+                case 1: { day = "Sunday"; break; }
+                case 2: { day = "Monday"; break; }
+                case 3: { day = "Tuesday"; break; }
+                case 4: { day = "Wednesday"; break; }
+                case 5: { day = "Thursday"; break; }
+                case 6: { day = "Friday"; break; }
+                case 7: { day = "Saturday"; break; }
+                default: { day = "Invalid Number"; break; }
+            }
+
+            Console.WriteLine($"day: {day}");
         }
     }
 }
