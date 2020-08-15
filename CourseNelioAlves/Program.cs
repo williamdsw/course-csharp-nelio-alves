598c686609070a907408c6214699c2c3413bedd1
williamdsw <williamdsw@outlook.com>
2020-08-15 13:21:03 -0300

    HashSet and SortedSet #2

--- CourseNelioAlves/Program.cs
+++ CourseNelioAlves/Program.cs
@@ -1,6 +1,7 @@
 ﻿
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace CourseNelioAlves
 {
@@ -10,24 +11,41 @@ namespace CourseNelioAlves
         {
 			try
 			{
-                HashSet<string> set = new HashSet<string>();
+                SortedSet<int> a = new SortedSet<int>() { 0, 2, 4, 5, 6, 8, 10 };
+                SortedSet<int> b = new SortedSet<int>() { 5, 6, 7, 8, 9, 10 };
 
-                set.Add("TV");
-                set.Add("Notebook");
-                set.Add("Tablet");
+                PrintCollection(a);
+                PrintCollection(b);
 
-                foreach (string item in set)
-                {
-                    Console.WriteLine(item);
-                }
+                // Union
+                SortedSet<int> c = new SortedSet<int>(a);
+                c.UnionWith(b);
+                PrintCollection(c);
 
-                Console.WriteLine($"Contains Notebook? {set.Contains("Notebook")}");
-                Console.WriteLine($"Contains Computer? {set.Contains("Computer")}");
+                // Intersection
+                SortedSet<int> d = new SortedSet<int>(a);
+                d.IntersectWith(b);
+                PrintCollection(d);
+
+                // Difference
+                SortedSet<int> e = new SortedSet<int>(a);
+                e.ExceptWith(b);
+                PrintCollection(e);
             }
 			catch (Exception ex)
 			{
                 Console.WriteLine(ex.Message);
 			}
+        }
+
+        private static void PrintCollection<T>(IEnumerable<T> collection)
+        {
+            foreach(T item in collection)
+            {
+                Console.Write($" {item} ");
+            }
+
+            Console.WriteLine();
         }
     }
 }
