af8db179609510990a18f0092af7bc951e292815
williamdsw <williamdsw@outlook.com>
2020-08-16 15:00:00 -0300

    Introduction to Delegates

--- CourseNelioAlves/Program.cs
+++ CourseNelioAlves/Program.cs
@@ -1,42 +1,47 @@
 ﻿
 using LastChapter.Entities;
+using LastChapter.Services;
 using System;
-using System.Collections.Generic;
 
 namespace CourseNelioAlves
 {
+    public delegate double BinaryNumericOperation(double n1, double n2);
+    public delegate double SingleNumericOperation(double n1);
+
     class Program
     {
-        private const int globalValue = 3;
-
         static void Main(string[] args)
         {
 			try
 			{
-                Test();
+                double x = 10;
+                double y = 12;
+                double max = CalculationService.Max(x, y);
+                double sum = CalculationService.Sum(x, y);
+                double square = CalculationService.Square(x);
+
+                Console.WriteLine($"x = {x}, y = {y}");
+                Console.WriteLine("----- COMMON -----");
+                Console.WriteLine($"max = {max}");
+                Console.WriteLine($"sum = {sum}");
+                Console.WriteLine($"square of x = {square}");
+
+                BinaryNumericOperation opMax = CalculationService.Max;
+                BinaryNumericOperation opSum = new BinaryNumericOperation(CalculationService.Sum);
+                SingleNumericOperation opSquare = CalculationService.Square;
+                max = opMax(x, y);
+                sum = opSum(x, y);
+                square = opSquare.Invoke(x);
+
+                Console.WriteLine("----- DELEGATES -----");
+                Console.WriteLine($"max = {max}");
+                Console.WriteLine($"sum = {sum}");
+                Console.WriteLine($"square of x = {square}");
             }
 			catch (Exception ex)
 			{
                 Console.WriteLine(ex.Message);
 			}
-        }
-
-        private static void Test()
-        {
-            int[] vect = new int[] { 3, 4, 5 };
-            ChangeOddValues(vect);
-            Console.WriteLine(string.Join(" ", vect));
-        }
-
-        private static void ChangeOddValues(int[] numbers)
-        {
-            for (int i = 0; i < numbers.Length; i++)
-            {
-                if (numbers[i] % 2 != 0)
-                {
-                    numbers[i] += globalValue;
-                }
-            }
         }
     }
 }
