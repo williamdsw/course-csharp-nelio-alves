74b0da692c7abba2034adc816fe20e48dfb102a2
williamdsw <williamdsw@outlook.com>
2020-08-16 17:24:23 -0300

    Solving an exercise with LINQ and Lambda expressions

---
+++ CourseNelioAlves/LastChapter/Entities/OtherProduct.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace CourseNelioAlves.LastChapter.Entities
+{
+    public class OtherProduct
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public OtherProduct(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, {Price.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
--- CourseNelioAlves/Program.cs
+++ CourseNelioAlves/Program.cs
@@ -1,7 +1,10 @@
 ﻿
+using CourseNelioAlves.LastChapter.Entities;
 using LastChapter.Entities;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 
 namespace CourseNelioAlves
@@ -12,72 +15,42 @@ namespace CourseNelioAlves
         {
 			try
 			{
-                Category catTools = new Category(1, "Tools", 2);
-                Category catComputers = new Category(2, "Computers", 1);
-                Category catElectronics = new Category(3, "Electronics", 1);
+                List<OtherProduct> products = new List<OtherProduct>();
 
-                List<Product> products = new List<Product>();
+                Console.Write("Enter full file path: ");
+                string path = Console.ReadLine();
 
-                products.Add(new Product(1, "Computer", 1100.0, catComputers));
-                products.Add(new Product(2, "Hammer", 90.0, catTools));
-                products.Add(new Product(3, "TV", 1700.0, catElectronics));
-                products.Add(new Product(4, "Notebook", 1300.0, catComputers));
-                products.Add(new Product(5, "Saw", 80.0, catTools));
-                products.Add(new Product(6, "Tablet", 700.0, catComputers));
-                products.Add(new Product(7, "Camera", 700.0, catElectronics));
-                products.Add(new Product(8, "Printer", 350.0, catElectronics));
-                products.Add(new Product(9, "MacBook", 1800.0, catComputers));
-                products.Add(new Product(10, "Sound Bar", 700.0, catElectronics));
-                products.Add(new Product(11, "Level", 70.0, catTools));
-
-                var result1 = from p in products
-                              where p.Category.Tier == 1 && p.Price < 900.0
-                              select p;
-                Print("TIER 1 AND PRICE < 900.0:", result1);
-
-                List<string> result2 = (from p in products
-                               where p.Category.Name == "Tools"
-                               select p.Name).ToList();
-                Print("NAMES OF PRODUCTS FROM TOOLS:", result2);
-
-                var result3 = from p in products
-                              where p.Name[0] == 'C'
-                              select new { p.Name, p.Price, CategoryName = p.Category.Name };
-                Print("NAMES STARTED WITH 'C' AND ANONYMOUS OBJECT:", result3);
-
-                List<Product> result4 = (from p in products
-                                         where p.Category.Tier == 1
-                                         orderby p.Price, p.Name
-                                         select p).ToList();
-                Print("TIER 1 ORDER BY PRICE THEN BY NAME", result4);
-
-                List<Product> result5 = (from p in result4 select p).Skip(2).Take(4).ToList();
-                Print("TIER 1 ORDER BY PRICE THEN BY NAME SKIP 2 TAKE 4", result5);
-
-                var result6 = (from p in products group p by p.Category);
-                foreach (IGrouping<Category, Product> group in result6)
+                using (StreamReader reader = File.OpenText(path))
                 {
-                    Console.WriteLine($"\nCategory {group.Key.Name}");
-                    foreach (Product p in group)
+                    while (!reader.EndOfStream)
                     {
-                        Console.WriteLine(p);
+                        string line = reader.ReadLine();
+                        string[] values = line.Split(',');
+                        string name = values[0];
+                        double price = double.Parse(values[1], CultureInfo.InvariantCulture);
+
+                        OtherProduct product = new OtherProduct(name, price);
+                        products.Add(product);
                     }
                 }
+
+                Console.WriteLine("\nALL PRODUCTS");
+                products.ForEach(p => Console.WriteLine(p));
+
+                double average = products.Select(p => p.Price).DefaultIfEmpty(0.00).Average();
+                Console.WriteLine($"\nAverage Price: {average.ToString("F2", CultureInfo.InvariantCulture)}");
+
+                // List<string> names = products.Where(p => p.Price < average).OrderByDescending(p => p.Name).Select(p => p.Name).ToList();
+                List<string> names = (from p in products
+                                      where p.Price < average
+                                      orderby p.Name descending
+                                      select p.Name).ToList();
+                names.ForEach(name => Console.WriteLine(name));
             }
-			catch (Exception ex)
+			catch (IOException ex)
 			{
                 Console.WriteLine(ex.Message);
 			}
-        }
-
-        private static void Print<T>(string message, IEnumerable<T> collection)
-        {
-            Console.WriteLine(message);
-            foreach (T item in collection)
-            {
-                Console.WriteLine(item);
-            }
-            Console.WriteLine();
         }
     }
 }
