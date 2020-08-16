04ff257730a64f756781dc460c4da797299ad985
williamdsw <williamdsw@outlook.com>
2020-08-16 14:49:26 -0300

    Functional programming and Lambda Calculus

--- CourseNelioAlves/Program.cs
+++ CourseNelioAlves/Program.cs
@@ -7,32 +7,13 @@ namespace CourseNelioAlves
 {
     class Program
     {
+        private const int globalValue = 3;
+
         static void Main(string[] args)
         {
 			try
 			{
-                List<Product> products = new List<Product>();
-
-                products.Add(new Product("TV", 900.00));
-                products.Add(new Product("Notebook", 1200.00));
-                products.Add(new Product("Tablet", 450.00));
-
-                // Only works if class model implements IComparable
-                //products.Sort();
-                //products.Sort(CompareProducts);
-
-                //Comparison<Product> comparison = CompareProducts;
-                //products.Sort(comparison);
-
-                //Comparison<Product> comparison = (p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
-                //products.Sort(comparison);
-
-                products.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()));
-
-                foreach (Product product in products)
-                {
-                    Console.WriteLine(product);
-                }
+                Test();
             }
 			catch (Exception ex)
 			{
@@ -40,9 +21,22 @@ namespace CourseNelioAlves
 			}
         }
 
-        private static int CompareProducts(Product p1, Product p2)
+        private static void Test()
         {
-            return p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
+            int[] vect = new int[] { 3, 4, 5 };
+            ChangeOddValues(vect);
+            Console.WriteLine(string.Join(" ", vect));
+        }
+
+        private static void ChangeOddValues(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 != 0)
+                {
+                    numbers[i] += globalValue;
+                }
+            }
         }
     }
 }
